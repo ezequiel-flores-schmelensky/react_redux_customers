a666de3d1975575e5fe16a44e02b44d6757bff9a
Ezequiel Flores <supereze.bxb@gamil.com>
2019-02-11 18:07:44 -0600

    redux-actions

--- src/actions/fetchCustomers.js
+++ src/actions/fetchCustomers.js
@@ -1,22 +1,7 @@
 import { FETCH_CUSTOMERS } from "./../constants";
 import { createAction } from "redux-actions";
 
-const customers = [
-    {
-        "dni": "adsd",
-        "name": "Juan Perez",
-        "age": 37
-    },
-    {
-        "dni": "sdfsdf",
-        "name": "Jotro",
-        "age": 37
-    },
-    {   
-        "dni": "fghgfh",
-        "name": "otro",
-        "age": 37
-    }
-];
+const url = 'http://localhost:3001/customers';
 
-export const fetchCustomers = createAction(FETCH_CUSTOMERS, () => customers); 
+const apiFetchCustomers = () => fetch(url).then(v => v.json());
+export const fetchCustomers = createAction(FETCH_CUSTOMERS, apiFetchCustomers); 
