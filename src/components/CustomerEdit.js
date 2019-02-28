65803e20953f25174a83ce9e337ceb7711de8d71
Ezequiel Flores <supereze.bxb@gamil.com>
2019-02-28 17:57:41 -0600

    validacion global

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -4,13 +4,27 @@ import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
 import { setPropsAsInitial } from '../helpers/setPropsAsInitial';
 
-const isRequired = value => (
+/*const isRequired = value => (
     !value && "Este campo es requerido"
-);
+);*/
 
 const isNumber = value => (
     isNaN(Number(value)) && "El campo debe ser un número"
 );
+
+const validate = values => {
+    const error = {};
+
+    if (!values.name) {
+        error.name = "El campo nombre es reauerido";
+    }
+
+    if (!values.dni) {
+        error.dni = "El campo nombre es reauerido";
+    }
+    
+    return error;
+};
 
 const MyField = ({input, meta, type, label, name}) => (
     <div>
@@ -31,20 +45,18 @@ const CustomerEdit = ({ name, dni, age }) => {
                     name="name" 
                     label="Nombre"
                     component={MyField} 
-                    type="text"
-                    validate={isRequired}></Field>
+                    type="text"></Field>
                 <Field 
                     name="dni" 
                     label="DNI"
-                    component={MyField} 
-                    type="text"
-                    validate={[isRequired, isNumber]}></Field>
+                    component={MyField}
+                    type="text"></Field>
                 <Field 
                     name="age"
                     label="Edad" 
                     component={MyField} 
-                    type="number"
-                    validate={isNumber}></Field>
+                    validate={isNumber}
+                    type="number"></Field>
             </form>
         </div>
     );
@@ -56,5 +68,5 @@ CustomerEdit.propTypes = {
     age: PropTypes.number,
 };
 
-const CustomerEditForm = reduxForm({ form: 'CustomerEdit' })(CustomerEdit);
+const CustomerEditForm = reduxForm({ form: 'CustomerEdit', validate })(CustomerEdit);
 export default setPropsAsInitial(CustomerEditForm);
