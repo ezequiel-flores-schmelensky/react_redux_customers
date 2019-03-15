8660a3b50849732fbbe1311711378b106788e784
Ezequiel Flores <supereze.bxb@gamil.com>
2019-03-14 23:18:11 -0600

    fix heroku

---
+++ build/precache-manifest.99c08c842b9a2aa42d8440f68a59a34a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9d958611f3663cbeed6f",
+    "url": "/static/js/main.9d958611.chunk.js"
+  },
+  {
+    "revision": "ae3f73a7d4e603397329",
+    "url": "/static/js/1.ae3f73a7.chunk.js"
+  },
+  {
+    "revision": "9d958611f3663cbeed6f",
+    "url": "/static/css/main.4eeb6bbf.chunk.css"
+  },
+  {
+    "revision": "79f38ecc40a2254eb32a53bea04564a4",
+    "url": "/index.html"
+  }
+];
