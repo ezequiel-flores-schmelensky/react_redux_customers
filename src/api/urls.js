8b94db74a396ad85fb4964ac6f53061f0a16cbed
Ezequiel Flores <supereze.bxb@gamil.com>
2019-03-14 22:54:22 -0600

    heroku

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,2 +1,4 @@
-const urlBase = 'http://localhost:3001/';
+//const urlBase = 'http://localhost:3001/'; https://react-service.herokuapp.com
+//const urlBase = 'http://localhost:8080/api/'
+const urlBase = 'https://react-service.herokuapp.com/api/'
 export const urlCustomers = `${urlBase}customers`;
